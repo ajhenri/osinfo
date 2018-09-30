33f8103ee3be27e95e143cebc3bf28ab548c4881
ajhenri <amandajohe@live.com>
2018-09-30 17:57:38 -0400

    correct to 2 spaces

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,28 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
-	entry: './renderer.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
-	},
-	target: 'electron-renderer',
-	module: {
-		rules: [
-			{
-				test: /\.js|.jsx?$/,
-				loader: 'babel-loader',
-				exclude: '/node_modules/',
-				options: {
-					presets: ['react']
-				}
-			},
-			{
-				test: /\.css$/,
-				loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
-			}
-		]
-	}
+  entry: './renderer.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  target: 'electron-renderer',
+  module: {
+    rules: [
+      {
+        test: /\.js|.jsx?$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+        options: {
+          presets: ['react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
+      }
+    ]
+  }
 };
 
 module.exports = config;
